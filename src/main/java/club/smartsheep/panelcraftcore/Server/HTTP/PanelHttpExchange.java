df10d8f3e11c6f604b39e4218ee19ae27ddec5fa
LittleSheep_ <cadenjiang@outlook.com>
2022-02-01 15:55:38 +0800

    add getQueryParameters API in the panel exchange

--- src/main/java/club/smartsheep/panelcraftcore/Server/HTTP/PanelHttpExchange.java
+++ src/main/java/club/smartsheep/panelcraftcore/Server/HTTP/PanelHttpExchange.java
@@ -7,27 +7,15 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PanelHttpExchange {
-    public enum HeaderTypes {
-        CONTENT_TYPE("Content-Type");
-
-        final String name;
-
-        HeaderTypes(String name) {
-            this.name = name;
-        }
-    }
-
     private final HttpExchange exchange;
-
-    private int statusCode = 200;
-
     public String Authorization_Code;
     public String Authorization_Username;
-
+    private int statusCode = 200;
     public PanelHttpExchange(HttpExchange exchange) {
         this.exchange = exchange;
     }
@@ -100,7 +88,26 @@ public class PanelHttpExchange {
         return exchange.getRemoteAddress().getHostString();
     }
 
-    public PanelHttpErrorSender getErrorSender() { return new PanelHttpErrorSender(this); }
+    public PanelHttpErrorSender getErrorSender() {
+        return new PanelHttpErrorSender(this);
+    }
+
+    public Map<String, String> getQueryParameters() {
+        String query = this.exchange.getRequestURI().getQuery();
+        if (query == null) {
+            return null;
+        }
+        Map<String, String> result = new HashMap<>();
+        for (String param : query.split("&")) {
+            String[] entry = param.split("=");
+            if (entry.length > 1) {
+                result.put(entry[0], entry[1]);
+            } else {
+                result.put(entry[0], "");
+            }
+        }
+        return result;
+    }
 
     public void send() throws IOException {
         OutputStream responseStream = this.exchange.getResponseBody();
@@ -128,5 +135,15 @@ public class PanelHttpExchange {
 
     public void send(Map<String, Object> data) throws IOException {
         this.send(new JSONObject(data).toString(), "application/json");
+    }
+
+    public enum HeaderTypes {
+        CONTENT_TYPE("Content-Type");
+
+        final String name;
+
+        HeaderTypes(String name) {
+            this.name = name;
+        }
     }
 }
